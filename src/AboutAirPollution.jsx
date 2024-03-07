6e58c6764f20a55d7fdb3c6104fcbc252cb8806e
Prijun Koirala <38245043+koiralaprijun@users.noreply.github.com>
2024-03-07 03:08:43 +0100

    50% of About Air Pollution and About Us Page

--- src/AboutAirPollution.jsx
+++ src/AboutAirPollution.jsx
@@ -1,105 +1,262 @@
 import React, { useRef, useEffect } from "react"
 import * as d3 from "d3"
+import { Box, Flex, Heading, Stack, Text } from "@chakra-ui/react"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon
+} from "@chakra-ui/react"
 
 import "../src/css/AboutAirPollution.css"
 
-const Header = () => (
-  <div>
-    <h1>How good is the weather?</h1>
-  </div>
-)
-
 export const ComparisonChart = () => {
-  const d3Container = useRef(null)
+  const svgRef = useRef(null)
 
   useEffect(() => {
-    if (d3Container.current) {
-      // Define data
-      const data = [
-        { name: "Human Hair", value: 70, color: "teal" }, // Value is hypothetical diameter scale
-        { name: "PM10", value: 10, color: "orange" },
-        { name: "PM2.5", value: 5, color: "maroon" }
+    const data = {
+      name: "root",
+      children: [
+        { name: "Human Hair", value: 70 },
+        { name: "PM10", value: 10 },
+        { name: "PM2.5", value: 5 }
       ]
-
-      // Define dimensions
-      const width = 600
-      const height = 300
-
-      // Create SVG container
-      const svg = d3
-        .select(d3Container.current)
-        .attr("width", width)
-        .attr("height", height)
-
-      // Create a scale for your circles
-      const scale = d3
-        .scaleSqrt()
-        .domain([0, d3.max(data, (d) => d.value)])
-        .range([0, width / 3])
-
-      // Remove previous circles if they exist
-      svg.selectAll("circle").remove()
-      svg.selectAll("text").remove()
-
-      // Create circles
-      const circles = svg
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", (d, i) => (i * width) / 3 + width / 6)
-        .attr("cy", height / 2)
-        .attr("r", (d) => scale(d.value))
-        .attr("fill", (d) => d.color)
-
-      // Add labels
-      svg
-        .selectAll("text")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("x", (d, i) => (i * width) / 3 + width / 6)
-        .attr("y", height / 2)
-        .attr("text-anchor", "middle")
-        .attr("dy", "0.35em") // to vertically center text
-        .text((d) => d.name)
-
-      // Add interactivity
-      circles
-        .on("mouseover", function (event, d) {
-          d3.select(this).attr("opacity", 0.7)
-          // You could also show a tooltip or change the circle size, etc.
-        })
-        .on("mouseout", function () {
-          d3.select(this).attr("opacity", 1)
-        })
     }
-  }, [])
 
-  return <svg ref={d3Container} />
+    const width = 600
+    const height = 300
+
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("text-anchor", "middle")
+
+    const root = d3
+      .hierarchy(data)
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value)
+
+    const pack = d3.pack().size([width, height]).padding(5)
+
+    pack(root)
+
+    const node = svg
+      .selectAll("g")
+      .data(root.descendants().slice(1))
+      .enter()
+      .append("g")
+      .attr("transform", (d) => `translate(${d.x},${d.y})`)
+
+    node
+      .append("circle")
+      .attr("r", (d) => d.r)
+      .attr("fill", (d) => {
+        if (d.data.name === "Human Hair") return "teal"
+        if (d.data.name === "PM10") return "orange"
+        if (d.data.name === "PM2.5") return "cyan"
+        return "none" // Fallback color
+      })
+
+    node
+      .append("text")
+      .text((d) => d.data.name)
+      .attr("dy", 4) // Slight adjustment to align text within the circle
+      .style("font-size", (d) => {
+        if (d.data.name === "Human Hair") return "18px" // Increase font size for Human Hair
+        if (d.data.name === "PM10" || d.data.name === "PM2.5") return "12px" // Decrease font size for PM10 and PM2.5
+        return "12px" // Default font size
+      })
+
+    node
+      .on("mouseover", function () {
+        d3.select(this).select("circle").style("fill-opacity", 0.95)
+      })
+      .on("mouseout", function () {
+        d3.select(this).select("circle").style("fill-opacity", 1)
+      })
+
+    return () => {
+      // Clean up D3 elements if needed
+    }
+  }, []) // Empty dependency array to run only once
+
+  return (
+    <div id="comparision-chart">
+      <svg ref={svgRef}></svg>
+    </div>
+  )
 }
 
-const AirQualityIndex = () => (
-  <div>{/* Map over an array of quality levels and descriptions */}</div>
-)
+const AirQualityIndex = () => {
+  const categories = [
+    {
+      name: "Good",
+      color: "#a8e05f",
+      description:
+        "Air quality is considered satisfactory, and air pollution poses little or no risk"
+    },
+    {
+      name: "Moderate",
+      color: "#fdd64b",
+      description:
+        "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+    },
+    {
+      name: "Unhealthy for Sensitive Groups",
+      color: "#f99049",
+      description:
+        "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    },
+    {
+      name: "Unhealthy",
+      color: "#f65e5f",
+      description:
+        "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+    },
+    {
+      name: "Very Unhealthy",
+      color: "#a070b6",
+      description:
+        "Health warnings of emergency conditions. The entire population is more likely to be affected."
+    },
+    {
+      name: "Hazardous",
+      color: "#a06a7b",
+      description:
+        "Health alert: everyone may experience more serious health effects"
+    }
+  ]
 
-const HealthImpact = () => (
+  return (
+    <Stack spacing={4} align="stretch">
+      <Heading as="h2" size="lg" p={4} bg="gray.100" textAlign="center">
+        How good is the weather?
+      </Heading>
+      {categories.map((category, index) => (
+        <Box key={index} bg={category.color} p={4} color="#3b3b3b">
+          <Heading as="h3" size="md">
+            {category.name}
+          </Heading>
+          <Text mt={2}>{category.description}</Text>
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+const UsefulLinks = () => (
   <div>{/* Map over an array of health impact items */}</div>
 )
 
-const FAQAccordion = () => <div>{/* FAQ items */}</div>
-
-const Footer = () => <div>{/* Footer content */}</div>
+const FAQAccordion = () => (
+  <div>
+    <Heading as="h3" size="md" my="4">
+      Air Pollutants
+    </Heading>
+    <Accordion allowToggle>
+      <AccordionItem>
+        <h2>
+          <AccordionButton _expanded={{ bg: "#3182CE", color: "white" }}>
+            <Box as="span" flex="1" textAlign="left">
+              What is PM?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem>
+        <h2>
+          <AccordionButton _expanded={{ bg: "#3182CE", color: "white" }}>
+            <Box as="span" flex="1" textAlign="left">
+              What is O2?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem>
+        <h2>
+          <AccordionButton _expanded={{ bg: "#3182CE", color: "white" }}>
+            <Box as="span" flex="1" textAlign="left">
+              What is NoX?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem>
+        <h2>
+          <AccordionButton _expanded={{ bg: "#3182CE", color: "white" }}>
+            <Box as="span" flex="1" textAlign="left">
+              What is Birch Pollen?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem>
+        <h2>
+          <AccordionButton _expanded={{ bg: "#3182CE", color: "white" }}>
+            <Box as="span" flex="1" textAlign="left">
+              What is AQI?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  </div>
+)
 
 const AboutAirPollution = () => (
-  <div>
-    <Header />
-    <ComparisonChart />
-    <AirQualityIndex />
-    <HealthImpact />
-    <FAQAccordion />
-    <Footer />
-  </div>
+  <Flex
+    direction={{ base: "column", md: "row" }}
+    justify="space-between"
+    className="main-container"
+  >
+    <Flex direction="column" className="first-col" p={4} flex="1">
+      <ComparisonChart />
+      <FAQAccordion className="faq-accordion" />
+    </Flex>
+    <Flex className="second-col" p={4} flex="1">
+      <AirQualityIndex />
+    </Flex>
+    <Flex className="third-col" p={4} flex="1">
+      <UsefulLinks />
+    </Flex>
+  </Flex>
 )
 
 export default AboutAirPollution
--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,22 +1,156 @@
-import React from "react"
+import {
+  Heading,
+  Avatar,
+  Box,
+  Center,
+  Text,
+  Stack,
+  Button,
+  Link,
+  Badge,
+  useColorModeValue,
+  Flex
+} from "@chakra-ui/react"
 
-const AboutUs = () => {
+const OurProfile = () => {
   return (
-    <div>
-      <h2>About Us</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum
-        risus nec justo fringilla, at consectetur elit dictum. Nullam commodo
-        magna sit amet urna lacinia, in scelerisque ex tincidunt. Vivamus
-        pretium orci eget justo vestibulum, in ultricies purus tempor. Sed eget
-        magna nec enim rhoncus fringilla. Sed id tortor at dui varius varius. In
-        hac habitasse platea dictumst. In hac habitasse platea dictumst. In hac
-        habitasse platea dictumst. In hac habitasse platea dictumst. In hac
-        habitasse platea dictumst. In hac habitasse platea dictumst. In hac
-        habitasse platea dictumst.
-      </p>
-    </div>
+    <Center py={6} mx="auto" my="0">
+      <Box
+        mx="4"
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar size={"xl"} src={"../public/img-jordan.jpg"} mb={4} />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          Jordan
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          jyjchian@kth.se
+        </Text>
+        <Text
+          textAlign={"center"}
+          color={useColorModeValue("gray.700", "gray.400")}
+          px={3}
+        >
+          45 Year experience in design industry. Looking for internship.
+        </Text>
+      </Box>
+
+      <Box
+        mx="4"
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar size={"xl"} src={"../public/img-prijun.jpg"} mb={4} />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          Prijun Koirala
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          kprijun@gmail.com
+        </Text>
+        <Text
+          textAlign={"center"}
+          color={useColorModeValue("gray.700", "gray.400")}
+          px={3}
+        >
+          45 Year experience in design industry. Looking for internship.
+        </Text>
+      </Box>
+
+      <Box
+        mx="4"
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar size={"xl"} src={"../public/img-tianyi.jpg"} mb={4} />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          Tianyi
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          tianyini@kth.se
+        </Text>
+        <Text
+          textAlign={"center"}
+          color={useColorModeValue("gray.700", "gray.400")}
+          px={3}
+        >
+          45 Year experience in design industry. Looking for internship.
+        </Text>
+      </Box>
+
+      <Box
+        mx="4"
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar size={"xl"} src={"../public/img-tingting.jpg"} mb={4} />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          Tingting
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          tinl@kth.se
+        </Text>
+        <Text
+          textAlign={"center"}
+          color={useColorModeValue("gray.700", "gray.400")}
+          px={3}
+        >
+          45 Year experience in design industry. Looking for internship.
+        </Text>
+      </Box>
+
+      <Box
+        mx="4"
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar size={"xl"} src={"../public/img-xinyue.jpg"} mb={4} />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          Xinyue
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          xinyhu@kth.se
+        </Text>
+        <Text
+          textAlign={"center"}
+          color={useColorModeValue("gray.700", "gray.400")}
+          px={3}
+        >
+          45 Year experience in design industry. Looking for internship.
+        </Text>
+      </Box>
+    </Center>
   )
 }
+
+const AboutUs = () =>
+  <Flex>
+    <OurProfile />
+  </Flex>
 
 export default AboutUs
